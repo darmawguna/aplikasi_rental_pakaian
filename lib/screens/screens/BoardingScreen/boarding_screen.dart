a420081f206958d4350bfc7844c734f32243d1cd
darmawguna <komangagus642@gmail.com>
2025-01-01 15:18:11 +0800

    update boarding screen

---
+++ lib/screens/screens/BoardingScreen/boarding_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class BoardingScreen extends StatefulWidget {
+  const BoardingScreen({super.key});
+
+  @override
+  State<BoardingScreen> createState() => _BoardingScreenState();
+}
+
+class _BoardingScreenState extends State<BoardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("Boarding Screen"));
+  }
+}
